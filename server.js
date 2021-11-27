ef726d72c5475ebc8a61eec2aa8896df85209dac
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-11-27 19:29:09 +1030

    get routes and middleware added to server.js

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ const PORT = process.env.PORT || 3001;
 // access JSON and urlencoded form input
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api)
 
 // access to public folder files
 app.use(express.static('public')); 
+
+// GET route for index.html
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET route for notes.html
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+// listening for localhost port
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
