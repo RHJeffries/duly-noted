94be22938959ca1ed244a87f683413835b65bf2b
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-11-27 19:07:12 +1030

    adding middleware to server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express'); 
 const path = require('path');
+
 // route folder
 const api = require('./routes/index.js');
-// to initialise express
+
+// to initialise express on server 3001
 const app = express(); 
+const PORT = process.env.PORT || 3001;
+
+// access JSON and urlencoded form input
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// access to public folder files
+app.use(express.static('public')); 
