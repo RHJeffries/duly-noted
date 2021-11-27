b89fdeb38a3ca7dbe877d698745c7e52410a53e9
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-11-27 21:59:47 +1030

    added routes to notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,29 @@
 const notes = require('express').Router();
-const fs = require('fs')
+const { readFile, readAppend };
+
+//unique id for notes
 const { uuid } = require('uuidv4');
 
+//GET route for retrieinvg all the notes and read from db.json 
+notes.get('/', (req, res) => {
+    readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST route for adding a new note
+notes.post('/', (req, res) => {
+    if (req.body) {
+
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid(),
+        }
+// append note and show if successful
+        readAppend(newNote, './db/db.json')
+        res.json('Note successfully added.')
+    } else {
+        res.json('There was an error adding the note.')
+    }
+})
 
 module.exports = notes;
